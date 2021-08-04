b4387e117405d46bbfb9232e0420252871a525f6
Max Jung <max.jung97@web.de>
2021-08-04 22:14:15 +0200

    fix: typo fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const API_URL =
     process.env.NODE_ENV === "development"
         ? "http://localhost:5080"
-        : "https://api.mx-sports.jmmedia-playground.de/";
+        : "https://api.mx-sports.jmmedia-playground.de";
