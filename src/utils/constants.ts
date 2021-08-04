e043ef227aac46600b73070b036b53faaf794077
Max Jung <max.jung97@web.de>
2021-08-04 22:11:03 +0200

    feat: added prod api domain

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,4 @@
-export const API_URL = 'http://localhost:5000';
+export const API_URL =
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:5080"
+        : "https://api.mx-sports.jmmedia-playground.de/";
